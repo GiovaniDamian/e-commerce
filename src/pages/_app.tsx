2d3a091d07e1a5ceb74c15d52eacc1a3b42e5d50
Giovani Damian <gedamian@gmail.com>
2024-07-27 16:21:07 -0300

    start work with payments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import { AppProvider } from '../data/context/AppContext'
 import { AuthProvider } from '../data/context/AuthContext'
+import StripeContext from '../data/context/StripeContext';
 
 function MyApp({ Component, pageProps }) {
     return (
         <AuthProvider>
             <AppProvider>
-                <Component {...pageProps} />
+                <StripeContext>
+                    <Component {...pageProps} />
+                </StripeContext>
             </AppProvider>
         </AuthProvider>
     )
